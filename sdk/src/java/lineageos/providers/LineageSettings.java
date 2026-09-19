5047e7f017529ebe6fb7f6f275885f3a71f6549e
Jonas Salo <salomannen1234@gmail.com>
2026-09-19 16:09:57 +0200

    sdk: allow additional Oplus touch polling modes
    Change-Id: I006f6f26acd5dee2467de26d95800ac48e56aea6

--- sdk/src/java/lineageos/providers/LineageSettings.java
+++ sdk/src/java/lineageos/providers/LineageSettings.java
@@ -885,15 +885,15 @@ public final class LineageSettings {
         public static final Validator NOTIFICATION_PLAY_QUEUE_VALIDATOR = sBooleanValidator;
 
         /**
-         * Touch polling rate mode.
-         * 0 = 120Hz, 1 = legacy on, 3 = 240Hz, 4 = 180Hz, 14 = 330Hz
+         * Touch polling rate mode. Values correspond to vendor report-rate mode indices and are
+         * device-specific. 0 is the default rate and 1 is the legacy enabled value.
          */
         public static final String HIGH_TOUCH_POLLING_RATE_ENABLE =
                 "high_touch_polling_rate_enable";
 
         /** @hide */
         public static final Validator HIGH_TOUCH_POLLING_RATE_ENABLE_VALIDATOR =
-                new DiscreteValueValidator(new String[] {"0", "1", "3", "4", "14"});
+                new DiscreteValueValidator(new String[] {"0", "1", "3", "4", "5", "12", "14"});
 
         /**
          * Whether the HighTouchSensitivity is activated or not.
